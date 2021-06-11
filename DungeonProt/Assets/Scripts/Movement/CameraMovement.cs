ecc1eff93e4761f54543911d9c686d8bab7050b7
Yaroslav Sidorkevich <72098175+SaiDoTech@users.noreply.github.com>
2021-06-11 13:38:17 +0300

    collider fix

--- DungeonProt/Assets/Scripts/Movement/CameraMovement.cs
+++ DungeonProt/Assets/Scripts/Movement/CameraMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public RoomManager RoomManager;
+    public float CameraSpeed = 3.55f;
+
     private Vector3 target;
 
     private void Update()
     {
         target = RoomManager.currentRoom.transform.position + new Vector3(0, 0, -10);
-        gameObject.transform.position = Vector3.Lerp(transform.position, target, 2.55f*Time.deltaTime);
+        gameObject.transform.position = Vector3.Lerp(transform.position, target, CameraSpeed*Time.deltaTime);
     }
 }
