ebad80129f18f8d414b5de51f76ffce11021726a
Yaroslav Sidorkevich <72098175+SaiDoTech@users.noreply.github.com>
2021-06-22 22:19:58 +0300

    Menu added

--- DungeonProt/Assets/Scripts/Menu.cs
+++ DungeonProt/Assets/Scripts/Menu.cs
@@ -5,24 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void BtPlayClicked()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Play()
-    {
-        SceneManager.LoadScene(0);
-    }
-
-    public void Quite()
+    public void BtQuiteClicked()
     {
         Application.Quit();
     }
