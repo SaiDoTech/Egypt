b82eb9b87e4823f4a67fe88f2bf23daa10fd5ce9
Yaroslav Sidorkevich <72098175+SaiDoTech@users.noreply.github.com>
2021-06-22 20:00:21 +0300

    Dash Fast Fix

--- DungeonProt/Assets/Scripts/Movement/DashScript.cs
+++ DungeonProt/Assets/Scripts/Movement/DashScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlinkScript : MonoBehaviour
+public class DashScript : MonoBehaviour
 {
     public float Cooldown;
     public float Distance;
@@ -50,8 +50,31 @@ public class BlinkScript : MonoBehaviour
     {
         trail.Play();
 
-        destination = CursorTransform.position;
+        var heading = CursorTransform.position - transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
 
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, direction, distance, layerMask);
+
+        if (hit.collider.gameObject.name == "Cursor")
+        {
+            destination = CursorTransform.position;
+        }
+        else
+        {
+            //destination.x = hit.point.x - 1.1f * (((CursorTransform.position.x - transform.position.x) * 0.3f) / (Mathf.Abs(CursorTransform.position.y - transform.position.y)));
+            //destination.y = hit.point.y - 1.1f * 0.3f * Mathf.Sign(CursorTransform.position.y - transform.position.y);
+
+            var dx = CursorTransform.position.x - transform.position.x;
+            var dy = Mathf.Abs(CursorTransform.position.y - transform.position.y);
+
+            if (dy < 1)
+                dy = 1;
+
+            destination.x = hit.point.x - ((dx * 0.3f) / dy);
+            destination.y = hit.point.y - 0.3f * Mathf.Sign(dy);
+        }
         blinking = true;
     }
 }
